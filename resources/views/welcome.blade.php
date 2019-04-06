0dbecf6337d9c27af58ead1638f48d40943ba482
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2019-04-06 12:27:57 +0300

    Clean ups

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,11 @@
 
         <title>BetIQ</title>
 
-        <!-- Fonts -->
+   
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-          <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <!-- Styles -->
+   
         <style>
             html, body {            
                
