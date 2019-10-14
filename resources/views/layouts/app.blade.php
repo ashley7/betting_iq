7f7480e5c09d32b7970ffaf48f6d106bfdd567e8
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2019-10-14 19:42:44 +0300

    Enhanced

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,6 +94,11 @@
         </nav>
 
         <main class="py-4">
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)                  
+                    <div class="text-danger">{{$error}}</div>              
+                @endforeach
+            @endif
             @yield('content')
         </main>
     </div>
